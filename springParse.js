2b419c51629bea848e670c60177df66195f6381f
SarahHale <id15she@cs.umu.se>
2018-02-23 21:11:49 +0100

    oasdoas

--- springParse.js
+++ springParse.js
@@ -19,20 +19,20 @@ function get_json(url, callback) {
 get_json("http://resource.sgu.se/oppnadata/data/grundvatten/kallor/kallor.json", (resp)=> {
     mutation{
       createSpring{
-        id: resp.features.properties.id ,
-        namn: resp.features.properties.name,
-        kommunkod: resp.features.properties.kommunkod,
-        lagesnoggrannhet:resp.features.properties.lagesnoggrannhet,
-        kalltyp: resp.features.properties.kalltyp,
-        akvifertyp: resp.features.properties.akvifertyp,
-        registrerad_datum:resp.features.properties.registrerad_datum,
-        observationsdatum: resp.features.properties.observationsdatum,
-        temp: resp.features.properties.temp,
-        matt_flode: resp.features.properties.matt_flode,
-        bedomt_flode: resp.features.properties.bedomt_flode,
-        ph: resp.features.properties.ph,
-        ledningsformaga: resp.features.properties.ledningsformaga,
-        notering: resp.features.properties.notering
+        id: id ,
+        namn: name,
+        kommunkod: kommunkod,
+        lagesnoggrannhet:lagesnoggrannhet,
+        kalltyp: kalltyp,
+        akvifertyp: akvifertyp,
+        registrerad_datum:registrerad_datum,
+        observationsdatum: observationsdatum,
+        temp: temp,
+        matt_flode: matt_flode,
+        bedomt_flode: bedomt_flode,
+        ph: properties.ph,
+        ledningsformaga: ledningsformaga,
+        notering: notering
       }
     }
 })
