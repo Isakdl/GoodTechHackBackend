e60b46ddd103986c906a5c67afce518b33895eaf
SarahHale <id15she@cs.umu.se>
2018-02-23 19:04:52 +0100

    new schema

--- src/hello.js
+++ src/hello.js
@@ -1,33 +1,11 @@
-var http = require('http');
-// ----receive function----v
-function get_json(url, callback) {
-    http.get(url, function(res) {
-        var body = '';
-        res.on('data', function(chunk) {
-            body += chunk;
-        });
 
-        res.on('end', function() {
-            var response = JSON.parse(body);
-// call function ----v
-            callback(response);
-        });
-    });
+export default async event => {
+  // you can use ES7 with async/await and even TypeScript in your functions :)
+  await new Promise(r => setTimeout(r, 50))
+
+  return {
+    data: {
+      message: `Hello ${event.data.name || 'World'}`
+    }
+  }
 }
-
-         // -----------the url---v         ------------the callback---v
-var mydata = get_json("http://resource.sgu.se/oppnadata/data/grundvatten/kallor/kallor.json", function (resp) {
-    console.log(resp);
-});
-
-// export default async event => {
-//   // you can use ES7 with async/await and even TypeScript in your functions :)
-//
-//   await new Promise(r => setTimeout(r, 50))
-//
-//   return {
-//     data: {
-//       message: `Hello ${event.data.name || 'World'}`
-//     }
-//   }
-// }
